d5934037a494df1b1cdd50618871b81734d496cb
stefanstefanovic94 <stefan.stef94@gmail.com>
2020-07-18 23:07:36 +0200

    Updates

---
+++ precache-manifest.eed86e2632a499db6a85dacc3e5e39f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962fa591ed1314203f576dc886909de2",
+    "url": "/Marvel/index.html"
+  },
+  {
+    "revision": "fa931dd658f4e5c5e444",
+    "url": "/Marvel/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dceaa411491eef1b0f82",
+    "url": "/Marvel/static/css/main.eaca8689.chunk.css"
+  },
+  {
+    "revision": "fa931dd658f4e5c5e444",
+    "url": "/Marvel/static/js/2.04ad2dc3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Marvel/static/js/2.04ad2dc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dceaa411491eef1b0f82",
+    "url": "/Marvel/static/js/main.6fde8299.chunk.js"
+  },
+  {
+    "revision": "f2b65c2e3087c002339f",
+    "url": "/Marvel/static/js/runtime-main.8703f331.js"
+  }
+]);
